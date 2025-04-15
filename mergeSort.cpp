d9b35c1e31531eecdac444156cc3accfff727808
ranggarap <fadhilahrangga26@gmail.com>
2025-04-15 11:25:02 +0700

    message array

--- mergeSort.cpp
+++ mergeSort.cpp
@@ -6,3 +6,31 @@ int arr [20], b[20];
 //n is size of array
 int n;
 
+// n is array input size
+int n;
+
+ void input ()
+ {
+while (true)
+    { 
+        cout << "masukkan panjang elemen array :";
+        cin >> n;
+
+        if (n< + 20)
+        {
+            break;
+        }
+        else
+        {
+            cout << " \nMaksimal panjang array adalah 20";
+
+        }
+    }
+    cout << " \n -----------"<< endl ;
+    cout << "\ninputkan isi element array" << endl;
+    cout << " \n---------------"<< endl;
+    
+
+
+
+ }
